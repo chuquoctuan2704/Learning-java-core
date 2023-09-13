2c9811df03156c98a8166f2bb5c45540b105606b
TuanCQ <77978869+chuquoctuan2704@users.noreply.github.com>
2023-09-13 03:52:55 +0000

    if condition

---
+++ IfCondition/IfCondition.java
@@ -0,0 +1,13 @@
+package IfCondition;
+
+public class IfCondition {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    if (a > b) {
+      System.out.println("An error occurred");
+    } else {
+      System.out.println("Right! a < b");
+    }
+  }
+}
