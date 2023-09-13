6fb7c95e674147c28b92f86438a7214f10c14d50
TuanCQ <77978869+chuquoctuan2704@users.noreply.github.com>
2023-09-13 03:17:32 +0000

    Arithmetic Operator

---
+++ ArithmeticOperatorsInJava/ArithmeticOperatorsInJava.java
@@ -0,0 +1,23 @@
+package ArithmeticOperatorsInJava;
+
+public class ArithmeticOperatorsInJava {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 4;
+
+    int sum = a + b;
+    System.out.println("sum = "+ sum);
+
+    int difference = a - b;
+    System.out.println("difference = "+ difference);
+
+    int product = a * b;
+    System.out.println("product = "+ product);
+
+    double quotient = (double) a / b;
+    System.out.println("quotient = "+ quotient);
+
+    int surplus = a % b;
+    System.out.println("surplus = "+ surplus);
+  }
+}
